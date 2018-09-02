5382b6ec8380045962bb30db47fef247382e5d4a
crossoverJie <crossoverjie@gmail.com>
2018-09-02 15:43:36 +0800

    :sparkles: Introducing new features.interceptorAdapter

---
+++ cicada-core/src/main/java/top/crossoverjie/cicada/server/intercept/AbstractCicadaInterceptorAdapter.java
@@ -0,0 +1,23 @@
+package top.crossoverjie.cicada.server.intercept;
+
+import top.crossoverjie.cicada.server.action.param.Param;
+
+/**
+ * Function:
+ *
+ * @author crossoverJie
+ *         Date: 2018/9/2 15:40
+ * @since JDK 1.8
+ */
+public abstract class AbstractCicadaInterceptorAdapter implements CicadaInterceptor{
+
+    @Override
+    public void before(Param param) {
+
+    }
+
+    @Override
+    public void after(Param param) {
+
+    }
+}
--- cicada-example/src/main/java/top/crossoverjie/cicada/example/Intercept/LoggerInterceptorAbstract.java
+++ cicada-example/src/main/java/top/crossoverjie/cicada/example/Intercept/LoggerInterceptorAbstract.java
@@ -1,9 +1,9 @@
 package top.crossoverjie.cicada.example.Intercept;
 
 import org.slf4j.Logger;
-import top.crossoverjie.cicada.server.intercept.CicadaInterceptor;
 import top.crossoverjie.cicada.server.action.param.Param;
 import top.crossoverjie.cicada.server.annotation.Interceptor;
+import top.crossoverjie.cicada.server.intercept.AbstractCicadaInterceptorAdapter;
 import top.crossoverjie.cicada.server.util.LoggerBuilder;
 
 /**
@@ -14,17 +14,13 @@ import top.crossoverjie.cicada.server.util.LoggerBuilder;
  * @since JDK 1.8
  */
 @Interceptor(value = "loggerInterceptor")
-public class LoggerInterceptor implements CicadaInterceptor{
+public class LoggerInterceptorAbstract extends AbstractCicadaInterceptorAdapter {
 
-    private static final Logger LOGGER = LoggerBuilder.getLogger(Interceptor.class) ;
+    private static final Logger LOGGER = LoggerBuilder.getLogger(LoggerInterceptorAbstract.class) ;
 
     @Override
     public void before(Param param) {
-        LOGGER.info("param=[{}]",param.toString());
+        LOGGER.info("logger param=[{}]",param.toString());
     }
 
-    @Override
-    public void after(Param param) {
-
-    }
 }
