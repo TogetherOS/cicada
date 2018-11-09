af2ddfc40bfee1db6f7faddc96e593ed8f75ee5a
crossoverJie <crossoverjie@gmail.com>
2018-11-09 22:23:35 +0800

    :loud_sound: Adding logs.

--- cicada-example/src/main/script/startup.sh
+++ cicada-example/src/main/script/startup.sh
@@ -7,4 +7,4 @@ kill -9 $id
 echo "killed $id"
 done
 
-nohup java -jar -Dlogback.configurationFile=/Users/chenjie/Documents/dev/github/cicada/cicada-example/src/main/resources/logback.xml cicada-example.jar > log.log 2>&1 &
+nohup java -jar cicada-example.jar > log.log 2>&1 &
