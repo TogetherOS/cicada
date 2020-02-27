1a7b5941feb295874086c3f49aec1eb2ef181d1c
crossoverJie <crossoverjie@gmail.com>
2020-02-28 01:03:52 +0800

    :bug: Fixing a bug.

--- cicada-core/src/main/java/top/crossoverjie/cicada/server/reflect/ClassScanner.java
+++ cicada-core/src/main/java/top/crossoverjie/cicada/server/reflect/ClassScanner.java
@@ -26,6 +26,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
